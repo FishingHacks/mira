81e4c6dccdd53dfa89d925f440221b1b4d486b1f
FishingHacks <fishinghacks@proton.me>
2025-08-17 01:34:04 +0200

    fix doc.sh

--- doc.sh
+++ doc.sh
@@ -3,24 +3,25 @@
 # generate clap alone -- bunching clap in with other dependencies for some reason stops cargo from being able to find it.
 cargo doc --no-deps -p clap
 cargo doc --open --no-deps \
- -p mira \
- -p mira-driver \
+ -p mira-argparse \
+ -p mira-typeck \
  -p mira-error-codes \
+ -p mira-driver \
  -p mira-target \
  -p mira-linking \
+ -p mira-parser \
+ -p mira-common \
  -p mira-errors \
  -p mira-lexer \
  -p mira-macros \
+ -p mira-llvm-backend \
  -p mira-progress-bar \
  -p mira-spans \
- -p mira-parser \
- -p mira-common \
  -p parking_lot \
  -p inkwell \
- -p termsize \
+ -p cfg-if \
  -p crossbeam-channel \
  -p syn \
  -p proc-macro2 \
  -p quote \
- -p cfg-if
-
+ -p termsize
